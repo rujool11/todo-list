e051e95075de9fd11822dc6126cb22b1ee389a23
rujool11 <132924872+rujool11@users.noreply.github.com>
2023-11-14 23:32:06 +0530

    adding tasks working

--- src/add.js
+++ src/add.js
@@ -8,19 +8,19 @@ const addProject = () => {
 
         let projDiv = document.createElement('div');
         projDiv.classList.add('task'); 
-        let title = document.createElement('h1');
+        let title = document.createElement('h3');
         title.textContent = `${newProjectObject.name}`; 
         projDiv.appendChild(title);
-        let desc = document.createElement('h2');
+        let desc = document.createElement('p');
         desc.textContent = `${newProjectObject.desc}`;
         projDiv.appendChild(desc);
 
-        let priority = document.createElement('h2');
+        let priority = document.createElement('p');
         priority.textContent = `${newProjectObject.priority}`;
         if (isUrgent(newProjectObject)){
-            priority.classList.add('red');
+            priority.classList.add('urgent');
         } else {
-            priority.classList.add('green');
+            priority.classList.add('normal');
         }
         projDiv.appendChild(priority);
         tasks.appendChild(projDiv);
